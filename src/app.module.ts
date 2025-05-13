7986177c429bca45c28e4ce96d05b16ee40c3718
Gabriel Silva <gabriel127.gk@gmail.com>
2025-05-12 23:38:28 -0300

    add: Colocando testes de integração e e2e no sistema. -> ok.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,43 @@ import { DeleteAllTransactionsUseCase } from './app/use-cases/delete-all-transac
 import { GetStatisticsUseCase } from './app/use-cases/get-statistics.use-case';
 import { TRANSACTIONS_REPOSITORY } from './infra/repositories/transactions.repository';
 
+import { LoggerModule } from 'nestjs-pino';
+import { IncomingMessage } from 'http';
+import pino from 'pino';
+
+const isDev = process.env.NODE_ENV === 'development';
+const isTest = process.env.NODE_ENV === 'test';
+
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        redact: ['req.headers.authorization'],
+        genReqId: (req: IncomingMessage) =>
+          (req.headers['x-request-id'] as string) || crypto.randomUUID(),
+
+        ...(isDev && {
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              translateTime: 'SYS:standard',
+              ignore: 'pid,hostname',
+            },
+          },
+        }),
+
+        ...(!isDev &&
+          !isTest && {
+            stream: pino.destination({
+              dest: 'logs/app.log',
+              mkdir: true,
+              sync: false,
+            }),
+          }),
+      },
+    }),
+  ],
   controllers: [TransactionsController, HealthController],
   providers: [
     {
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
+  app.useLogger(app.get(Logger));
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
@@ -23,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
